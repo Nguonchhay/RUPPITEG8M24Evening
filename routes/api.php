f3bb1480c7cfda7f83298f6bb82e41872f208f61
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-05-09 20:26:08 +0700

    [TASK] Middleware with login role

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::get('/user', function (Request $request) {
 Route::post('/auth/login', [App\Http\Controllers\Api\LoginAPIController::class, 'login']);
 
 Route::get('/categories', [App\Http\Controllers\Api\CategoryAPIController::class, 'index']);
-Route::post('/categories', [App\Http\Controllers\Api\CategoryAPIController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/categories', [App\Http\Controllers\Api\CategoryAPIController::class, 'store'])->middleware('auth:sanctum')->middleware(\App\Http\Middleware\AdminMiddleware::class);;
 Route::put('/categories/{category}/update', [App\Http\Controllers\Api\CategoryAPIController::class, 'update']);
 Route::delete('/categories/{category}/delete', [App\Http\Controllers\Api\CategoryAPIController::class, 'destroy']);
